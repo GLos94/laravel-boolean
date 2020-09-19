d66387662af4e1b0c1ad0320a093e513659b4dd4
Giulia <giulialosito94@gmail.com>
2020-09-19 18:07:02 +0200

    Footer

--- MyLaravel2/resources/views/layouts/main-layout.blade.php
+++ MyLaravel2/resources/views/layouts/main-layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Laravel Boolean</title>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
